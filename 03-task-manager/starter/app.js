28e4d8e2f85be142aef4c0c70f3d7b0d9e8872a2
raul-2001 <rlqv2001@gmail.com>
2024-03-27 01:22:35 -0400

    lesson 5

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,35 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+const tesks = require('./routes/tasks');
+const connectDB = require('./db/connect');
+require('dotenv').config();
+
+const port = 3000;
+
+// middleware
+app.use(express.json());
+
+//routes
+app.get('/hello', (req, res) => {
+    res.send('Task Manager App')
+})
+
+app.use('/api/v1/tasks', tesks);
+
+// app.get('/api/v1/tasks')         - get all the tasks
+// app.post('/api/v1/tasks')        - create a new task
+// app.get('/api/v1/tasks/:id')     - get single task
+// app.patch('/api/v1/tasks/:id')   - update task
+// app.delete('/api/v1/tasks/:id')  - delete task
+
+const start = async () => {
+    
+    try {
+        await connectDB(process.env.MANGO_URI)
+        app.listen(port, console.log(`Server is listening on port ${port}...`))
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+start();
