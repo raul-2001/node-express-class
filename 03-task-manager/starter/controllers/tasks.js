6e6c701b5b2ba8bd0c407fa4ced5117ec5546ff4
raul-2001 <rlqv2001@gmail.com>
2024-04-03 10:04:25 -0400

    week6

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,25 +1,57 @@
 const Task = require('../models/Task');
+const asyncWrapper = require('../middleware/async');
+const {createCustomError} = require('../errors/custom-error')
 
-const getAllTasks = (req, res) => {
-    res.status(201).json({ success: true, msg: "All tasks" })
-}
+const getAllTasks = asyncWrapper(async (req, res) => {
+    const tasks = await Task.find({});
+    res.status(201).json({ tasks });
+})
 
-const createTask = async (req, res) => {
+const createTask = asyncWrapper (async (req, res) => {
+
     const task = await Task.create(req.body)
     res.status(201).json({ task: task })
-}
 
-const getSingleTask = (req, res) => {
-    res.status(201).json({ success: true, msg: "Single task" })
-}
+})
 
-const updateTask = (req, res) => {
-    res.status(201).json({ success: true, msg: "Task is updated", id: req.params.id })
-}
+const getSingleTask = asyncWrapper( async (req, res, next) => {
 
-const deleteTask = (req, res) => {
-    res.status(201).json({ success: true, msg: "Task is deleted", id: req.params.id })
-}
+    const {id: TaskID} = req.params;
+    const task = await Task.findOne({_id: TaskID});
+    if (!task) {
+        // we creating here a new custom error class and it extended js error (we create new instance)
+        return next(createCustomError(`No taks whith id: ${TaskID}`, 404))
+    }
+    res.status(200).json({ task })
+
+})
+
+
+const deleteTask = asyncWrapper( async (req, res) => {
+
+    const {id: TaskID} = req.params;
+    const task = await Task.findOneAndDelete({_id: TaskID});
+    if (!task) {
+        return res.status(404).json({msg: `No taks whith id: ${TaskID}`});
+    }
+
+    res.status(200).json({ task });
+
+    
+})
+
+const updateTask = asyncWrapper( async (req, res) => {
+
+    const {id: TaskID} = req.params;
+    const task = await Task.findOneAndUpdate({_id: TaskID}, req.body, {
+        new: true,
+        runValidators: true,
+    })
+
+    res.status(200).json({ task });
+
+})
+
 
 module.exports = {
     getAllTasks, 
