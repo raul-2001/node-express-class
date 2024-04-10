a86b286fb3966c465baef3c772280f22bac56eef
raul-2001 <rlqv2001@gmail.com>
2024-04-10 10:30:19 -0400

    week7

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,1 +1,44 @@
-console.log('04 Store API')
+require('dotenv').config();
+// async errors
+
+const express = require('express');
+const app = express();
+const connectDB = require('./db/connect');
+
+const productsRouter = require('./routes/products');
+
+const notFoundMiddlware = require('./middleware/not-found');
+const errorMiddleware = require('./middleware/error-handler');
+
+// middleware
+app.use(express.json());
+
+// routes
+
+app.get('/', (req, res) => {
+    res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
+});
+
+app.use('/api/v1/products', productsRouter);
+
+
+// product routes
+
+app.use(notFoundMiddlware);
+app.use(errorMiddleware);
+
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MANGO_URI);
+        app.listen(port, console.log(`Server is listening port ${port}...`));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
+
+
